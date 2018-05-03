9878a0dbdad2445407b8b4acf3bcd969d8063495
Adi Andrea <dekz.itfz@gmail.com>
2018-05-03 16:35:08 +0800

    setup aaptOptions to disable compress on tflite file, add rx lib to handle threading

--- app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
+++ app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package dekz.id.tensorflowandroid;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +15,25 @@ import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+import java.io.IOException;
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MODEL_PATH = "mobilenet_quant_v1_224.tflite";
+    private static final String LABEL_PATH = "labels.txt";
+    private static final int INPUT_SIZE = 224;
+
+    private Classifier classifier;
+    private CompositeDisposable compositeDisposable;
 
     private CameraView cameraView;
     private Button btnCapture;
@@ -22,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        compositeDisposable = new CompositeDisposable();
 
         cameraView = findViewById(R.id.camera);
         btnCapture = findViewById(R.id.btn_capture);
@@ -40,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
                 Log.d("CameraKitListener", "image captured!");
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                //imageViewResult.setImageBitmap(bitmap);
+                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+                Log.i("RESULTS ----> ", results.toString());
+                //textViewResult.setText(results.toString());
             }
 
             @Override
@@ -54,6 +82,23 @@ public class MainActivity extends AppCompatActivity {
                 cameraView.captureImage();
             }
         });
+
+        initTensorFlow().subscribe(new CompletableObserver() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                compositeDisposable.add(d);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i("initTensorFlow", "complete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("initTensorFlow", e.getMessage());
+            }
+        });
     }
 
     @Override
@@ -66,5 +111,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         cameraView.stop();
         super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        closeClassifier().subscribe(new CompletableObserver() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                compositeDisposable.add(d);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i("closeClassifier","completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("closeClassifier", e.getMessage());
+            }
+        });
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+
+    private Completable closeClassifier(){
+        return Completable.fromAction(new Action() {
+            @Override
+            public void run() {
+                classifier.close();
+            }
+        }).subscribeOn(Schedulers.newThread());
+    }
+
+    private Completable initTensorFlow(){
+        return Completable.fromAction(new Action() {
+            @Override
+            public void run() throws IOException {
+                classifier = TensorFlowImageClassifier.create(
+                        getAssets(),
+                        MODEL_PATH,
+                        LABEL_PATH,
+                        INPUT_SIZE);
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread());
     }
 }

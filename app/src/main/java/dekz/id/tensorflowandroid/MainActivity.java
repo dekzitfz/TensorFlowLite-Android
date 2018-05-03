b7d916821b2eff2f08c68807afaea31dc3692788
Adi Andrea <dekz.itfz@gmail.com>
2018-05-03 16:00:44 +0800

    fix version conflict by downgrade supp lib

--- app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
+++ app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
@@ -2,12 +2,21 @@ package dekz.id.tensorflowandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraView cameraView;
+    private Button btnCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cameraView = findViewById(R.id.camera);
+        btnCapture = findViewById(R.id.btn_capture);
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                Log.d("CameraKitListener", "image captured!");
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.captureImage();
+            }
+        });
     }
 
     @Override

ae309ba21f4ad94e5c45eed1dddbea4b20dd1654
Adi Andrea <dekz.itfz@gmail.com>
2018-05-03 15:49:06 +0800

    setup camera

--- app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
+++ app/src/main/java/dekz/id/tensorflowandroid/MainActivity.java
@@ -3,11 +3,29 @@ package dekz.id.tensorflowandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wonderkiln.camerakit.CameraView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraView = findViewById(R.id.camera);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
     }
 }
